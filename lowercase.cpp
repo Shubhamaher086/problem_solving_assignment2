11f2843fefc167e6e3f2936e9079a61833541f22
Shubhamaher086 <97600988+Shubhamaher086@users.noreply.github.com>
2022-10-19 16:03:19 +0530

    Update lowercase.cpp

--- lowercase.cpp
+++ lowercase.cpp
@@ -1,4 +1,4 @@
-//Write a program to accept a string and convert all its characters to lower case.
+// Write a program to accept a string and convert all its characters to lower case.
 
 #include <iostream>
 using namespace std;
@@ -6,18 +6,24 @@ int main()
 {
 
     string str;
-
+    int i;
     cout << "Enter a string : ";
-    getline(cin, str);                              //Accept and read the string
+    getline(cin, str);                              // Accept and read the string
 
-    for (int i = 0; i < str.length(); i++)          //traverse the string
+    for (i = 0; i < str.length(); i++)              // traverse the string
     {
-        str[i] = str[i]+32;                         //change to upper case by adding 32
+        if (!(str[i] >= 'a' || str[i] >= 'z'))      // check if characters of string already in lowercase
+        {
+            str[i] = str[i] + 32;                   // change to upper case by adding 32
+        }
+        else
+        {
+            str[i] = str[i];
+        }
     }
 
-    cout <<endl;
-    cout <<"New string : " << str <<endl;             //print string
-    
+    cout << endl;
+    cout << "New string : " << str << endl;         // print string
 
     return 0;
 }
